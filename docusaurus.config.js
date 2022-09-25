db59892512b51945e2a62245b8947f51688220f7
rxliuli <rxliuli@gmail.com>
2022-09-25 13:15:18 +0800

    chore: 禁用 serach

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,22 +142,22 @@ const config = {
       },
     }),
   themes: [
-    [
-      require.resolve('@easyops-cn/docusaurus-search-local'),
-      {
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        language: ['en', 'zh'],
-        // ```
-        indexDocs: true,
-        indexBlog: false,
-        docsRouteBasePath: 'books',
-        docsDir: 'books',
-      },
-    ],
+    // [
+    //   require.resolve('@easyops-cn/docusaurus-search-local'),
+    //   {
+    //     // ... Your options.
+    //     // `hashed` is recommended as long-term-cache of index file is possible.
+    //     hashed: true,
+    //     // For Docs using Chinese, The `language` is recommended to set to:
+    //     // ```
+    //     language: ['en', 'zh'],
+    //     // ```
+    //     indexDocs: true,
+    //     indexBlog: false,
+    //     docsRouteBasePath: 'books',
+    //     docsDir: 'books',
+    //   },
+    // ],
   ],
 }
 
